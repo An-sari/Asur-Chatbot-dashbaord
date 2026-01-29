f9a85c2a1a968e8f58fbeb4f057fabaec980f621
AnsuryX <Ayoubabdishakur@gmail.com>
2026-01-29 20:19:06 +0300

    built the app locaalu

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-js';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
